c5cb883b82e3075c1d94c5063fd7b26b6c7c542d
Abhijit Suprem <asf2182@columbia.edu>
2017-09-10 14:45:06 -0400

    bug fixes

--- neo-server-test.py
+++ neo-server-test.py
@@ -7,20 +7,20 @@ driver = GraphDatabase.driver(uri, auth=("neo4j", "scientia"))
 
 #spo - s: subject; v: verb/predicate/preposition; o: object
 
-def exact-spo(e_subject, e_predicate, e_object):
+def exact_spo(e_subject, e_predicate, e_object):
     with driver.session() as session:
         with session.begin_transaction() as tx:
             for record in tx.run(   "match (s:Object)-[:SUBJ]->(p:Relation)-[:OBJ]->(o:Object) "
-                                    "where s.synset={e_subject} "
-                                    "where p.synset={e_predicate} "
-                                    "where o.synset={e_object} "
+                                    "where s.synset={e_subject} and "
+                                    " p.synset={e_predicate} and "
+                                    " o.synset={e_object} "
                                     "return s,p,o", 
                                     e_subject=e_subject, 
                                     e_object=e_object, 
-                                    e_predicate=e_predicate)
+                                    e_predicate=e_predicate):
                 pdb.set_trace()
                 #print(record["b.name"])
                     
 if __name__ == "__main__":
-    exact-spo('leg.n.01', 'under.r.01', 'counter.n.01')
+    exact_spo('leg.n.01', 'under.r.01', 'counter.n.01')
 
