10fa9a1f667fdb5f48247b634529208044aefe25
Abhijit Suprem <asf2182@columbia.edu>
2017-10-06 17:05:40 -0400

    fixed the ranking issue

--- synset_explore.py
+++ synset_explore.py
@@ -23,20 +23,31 @@ def initialize():
 
 
 def get_family_rankings(word_list):
+    hypo_extended = set()
+    hyper_extended = set()
+    hypo_ranks = []
+    hyper_ranks = []
     family = set()
     list_ranks=[]
     for word in word_list:
         hypo_family = set([i for i in word.closure(hypo)])
         hyper_family = set([i for i in word.closure(hyper)])
-        this_family = hypo_family | hyper_family
-        family |= this_family
-
-        this_family.add(word)
-        this_ranking = {i:word.lch_similarity(i) for i in this_family}
-        list_ranks.append(sorted(this_ranking.items(), key=operator.itemgetter(1), reverse=True))
+        #this_family = hypo_family | hyper_family
+        #family |= this_family
+        hypo_extended |= hypo_family
+        hyper_extended |= hyper_family
+        #family.add(word)
+        family |= hypo_family
+        family |= hyper_family
+        #this_family.add(word)
+        hypo_ranking = {str(i)[8:-2]:word.lch_similarity(i) for i in hypo_family}
+        hyper_ranking = {str(i)[8:-2]:word.lch_similarity(i) for i in hyper_family}
+        #list_ranks.append(sorted(this_ranking.items(), key=operator.itemgetter(1), reverse=True))
+        hypo_ranks.append(sorted(hypo_ranking.items(), key=operator.itemgetter(1), reverse=True))
+        hyper_ranks.append(sorted(hyper_ranking.items(), key=operator.itemgetter(1), reverse=True))
 
     family_list = [str(member)[8:-2] for member in family]
-    return family_list, list_ranks
+    return family_list, hypo_ranks, hyper_ranks
 
 def get_mains(word_list):
     return [str(member)[8:-2] for member in word_list]
@@ -49,7 +60,7 @@ def main():
     while 1:
         word = raw_input('word:   ')
         word_list = get_synset(word.replace(' ','_'))
-        family, list_ranks = get_family_rankings(word_list)
+        family, hypo_ranks, hyper_ranks = get_family_rankings(word_list)
         mains = get_mains(word_list)
         family_synset_counts = get_counts(family, sqCurs)
         main_synset_counts = get_counts(mains,sqCurs)
@@ -58,9 +69,31 @@ def main():
         # FIGURE OUT A WAY TO EXTRACT MORE RELEVANT RANKIGNS OVER LESS RELEVANT RANKINGS
 
 
-        rank_list=[]
+        #rank_list=[]
         in_dict={}
+        rank_list=[]
+        hypo_list = sorted([item for hypo in hypo_ranks for item in hypo],key=lambda x:x[1],reverse=True)
+        hyper_list = sorted([item for hyper in hyper_ranks for item in hyper],key=lambda x:x[1],reverse=True)
 
+        for entry in word_list:
+            if str(entry)[8:-2] in main_synset_counts and str(entry)[8:-2] not in in_dict:
+                rank_list.append((str(entry)[8:-2],main_synset_counts[str(entry)[8:-2]]))
+                in_dict[str(entry)[8:-2]]=1
+        for entry in hypo_list:
+            if entry[0] in family_synset_counts and entry[0] not in in_dict:
+                rank_list.append((entry[0],family_synset_counts[entry[0]]))
+                in_dict[entry[0]]=1
+        for entry in hyper_list:
+            if entry[0] in family_synset_counts and entry[0] not in in_dict:
+                rank_list.append((entry[0],family_synset_counts[entry[0]]))
+                in_dict[entry[0]]=1
+
+        for entry in rank_list:
+            print entry[0] + ' : ' + str(entry[1])
+
+        #for entry in 
+        #pdb.set_trace()
+        '''
         for idx,word in enumerate(word_list):
             rank_dict={}
             #THIS IS THE MAINRANKINGS
@@ -83,7 +116,7 @@ def main():
         for entry in rank_list:
             print entry[0] + ' : ' + str(entry[1])
         pdb.set_trace()
-
+        '''
 
         print '\n\n\n-------------------------------\n'
     
