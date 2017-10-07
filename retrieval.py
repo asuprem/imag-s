e5e7806e9db2ee50a0a4102482d4d6d751600d42
Abhijit Suprem <asf2182@columbia.edu>
2017-10-07 01:02:38 -0400

    first part of approximate matching - can sort of get the aggregate relations approxmates from a subject

--- retrieval.py
+++ retrieval.py
@@ -1,22 +1,76 @@
 from neo4j.v1 import GraphDatabase
 import pdb
-import approximate_utils
-'''
+from synset_explorer import Synset_Explorer
+#import approximate_utils
+
 uri = "bolt://localhost:7687"
 driver = GraphDatabase.driver(uri, auth=("neo4j", "scientia"))
-'''
+
 # RUN by python retrieval.py query1.query
+
+def clauseJoin(matchClause,conditionClause,returnClause):
+    return matchClause+' '+conditionClause+' '+returnClause
+
+def subject_relations_approximates(subjects):
+    matchClause = 'match (n:aggregateObject)-[:SUBJ]->(r:aggregateRelation)'
+    conditionClause = 'where s.synset in '+str(subjects)
+    returnClause = 'return r'
+    
+    fullClause = clauseJoin(matchClause,conditionClause,returnClause)
+    
+    with driver.session() as session:
+        with session.begin_transaction() as tx:
+            result = tx.run(fullClause)
+    pdb.set_trace()
+
+
+
+
 
 
 def main():
 
-    query_file = sys.argv[1]
+    objects = Synset_Explorer('../ExtractedData/objects.db')
+    relations = Synset_Explorer('../ExtractedData/relations.db')
+    #query_file_name = sys.argv[1]
+    query_file_name = 'queries/query2.query'
+    query_file = open(query_file_name,'r')
+
+    nouns = {}
+    predicates = {}
+
+    #Get the relations and nouns
+    for line in query_file:
+        line = line.strip().split(',')
+        if line[1] == 'n':
+            nouns[int(line[0])] = line[2]
+        if line[1] == 'r':
+            predicates[int(line[0])] = (line[2],int(line[3]), int(line[4]))
+    
+    relations = []
+    for entry in predicates:
+        relations.append((nouns[predicates[entry][1]],predicates[entry][0],nouns[predicates[entry][2]]))
+
+    #----------------------------------------------------------------------------#
+    # USE the relation component approximates to generate relation approximates
+    queryApproximates=[]
+    for relation in relations:
+        subjectFamily = objects.explore(relation[0])
+        objectFamily = objects.explore(relations[1])
+        pdb.set_trace()
+
+        
 
 
 
 
 
-    #exact_spo(e_subject, e_predicate, e_object)
+    
+
+
+
+
+
 
 if __name__ == "__main__":
     main()
